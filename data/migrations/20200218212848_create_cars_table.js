79709bf53af0f23eae22b96d66530ed791f687e6
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-18 21:30:53 -0500

    [Add] Fixed db and added new migrations.

---
+++ data/migrations/20200218212848_create_cars_table.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.string('make', 256).notNullable().index();
+        tbl.string('model', 256).notNullable().index();
+        tbl.string('transmission_type', 256).nullable();
+        tbl.string('title_status', 256).nullable();
+        tbl.integer('mileage').notNullable();
+        tbl.string('vin_num').notNullable();
+        tbl.integer('year').notNullable();
+        tbl.timestamps();
+    });
+  };
+  
+  //undo changes
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
+  };
+  
