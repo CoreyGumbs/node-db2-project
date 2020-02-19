ec41d0f75a734005f9cbbf8146bb32ed81db2550
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-18 22:16:59 -0500

    [Add] Added Update and Delete endpoints, postman tested and working

--- router/router.js
+++ router/router.js
@@ -30,7 +30,7 @@ router.post('/cars', (req, res) => {
     db('cars')
     .insert(req.body, 'id')
     .then(ids => {
-        res.status(201).json(ids);
+        res.status(201).json({message: `New car listing was created successfully. listingID: ${ids}`});
     })
     .catch(error => {
         res.status(500).json({error: "There was a problem adding the car."});
@@ -42,12 +42,25 @@ router.put('/cars/:id', (req, res) => {
     .where({id: req.params.id})
     .update(req.body)
     .then(car => {
-        res.status(200).json(car);
+        res.status(200).json({message: `Listing #${req.param.id} was updated.`});
     })
     .catch(error => {
         res.status(500).json({error: "There wasa problem updating entry"});
     })
 
-})
+});
+
+router.delete('/cars/:id', (req, res) => {
+
+    db('cars')
+    .where({id: req.params.id})
+    .del()
+    .then(car => {
+        res.status(200).json({message: `Car listing #${req.params.id} was deleted.`})
+    })
+    .catch(error => {
+        res.status(500).json({error: 'There was a problem deleting the listing.'});
+    });
+});
 
 module.exports =  router;
